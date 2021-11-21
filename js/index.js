eae2276734094450683123c00546c65d89c99bcc
cwyADT107102 <68813383+cwy104seung28@users.noreply.github.com>
2021-11-21 17:57:30 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ function getAuthorizationHeader() {
 function cityResponse() {
     let city = document.getElementById('text').value;
     console.log(city);
+    
 }
 cityResponse();
 function mapResponse() {
